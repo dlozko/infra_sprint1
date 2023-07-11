1c0197c225a41c36927f7761068d8856018ed00e
Denis Lozko <118303678+dlozko@users.noreply.github.com>
2023-07-11 15:02:36 +0300

    Update settings.py

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET', default=None)
+SECRET_KEY = os.getenv('SECRET', default="SECRET")
 
 DEBUG = False
 
